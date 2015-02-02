67c85f8de044f35a2efd5108b49659c6b1a94100
Victor Chan <victor.z.chan@gmail.com>
2015-02-02 19:59:43 +1100

    Write test stubs

--- CaffineFix/MyPlayground.playground/section-1.swift
+++
@@ -1,5 +0,0 @@
-// Playground - noun: a place where people can play
-//TODO remove this file
-import Cocoa
-
-var str = "Hello, playground"
--- CaffineFixTests/CaffineFixTests.swift
+++ CaffineFixTests/CaffineFixTests.swift
@@ -8,29 +8,56 @@
 
 import UIKit
 import XCTest
+import CaffineFix
 
 class CaffineFixTests: XCTestCase {
+    let testLL = "37.33240905,-122.03051211"
+    // Can create 
+    let shopidWithPic = "4bf58dd8d48988d1e0931735"
+    let shopidWithoutPic = "4f4eb369e4b0e52480690902"
+    let shopidWithContact = "4e4dd3f8bd41b76bef93cbf0"
+    let shopidWithoutContact = "4dd1a2d652b15d0acc6c89dd"
+    
+    let coffeeListTableViewController = CoffeeListTableViewController()
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testInitializing(){
+        XCTAssertNotNil(coffeeListTableViewController, "Coffee List Table View Controller did not load")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testGettingVenuesFromServer(){
+        XCTAssertNotNil(coffeeListTableViewController.exploreUrl(testLL), "Explore URL fail to create")
+        
     }
     
+    func testExtractingVenuesFromJson(){
+        //Using mock json, test if venue can be extracted properly using extractVenue function
+    }
+    
+    func testDisplayVenueInCoffeeEntryTableViewCell(){
+        //Using mock venues, test if cells display correctly
+    }
+    
+    func testGetVenueInfoFromServer(){
+        //test if url is created correctly
+        //test if venue is retrieved from server
+    }
+    
+    func testExtractContactDetailsFromJson(){
+        //using mock json, test if retrieve contact details
+    }
+    
+    func testDisplayVenueDetailInCoffeeDetailTableViewController(){
+        //Using mock venues, test if venues are displayed correctly.
+    }
 }
